291815927cb9f6bce3364048ade89bd215573969
poonam36 <poonam63.kumari36@gmail.com>
2022-07-29 10:53:28 +0530

    second commit

---
+++ src/common/header/Header.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import logo from 'D:/upGrad/reactProjects/movies-app/src/assets/logo.svg';
+import './Header.css';
+class Header extends Component {
+ render() {
+ return (
+ <div className="header">
+ <img src={logo} className="headerlogo" alt="logo" />
+ </div>
+ );
+ }
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header from './common/header/Header';
+//import logo from '.assets/logo.svg';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- 
-);
+  <React.StrictMode>
+  <Header/>
+  
+</React.StrictMode>
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
